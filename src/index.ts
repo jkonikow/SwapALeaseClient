ca898408d40071884e88c003ec2a30de0ca1cb40
Jkon1513 <jkon1513@gmail.com>
2022-06-25 21:31:55 -0700

    Expose VehicleInfo and LeaseInfo

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { default as SwapALeaseClient } from "./client/SwapALeaseClient";
 export { default as GetListingsRequest } from "./client/GetListingsRequest";
 export { default as GetListingsResponse } from "./client/GetListingsResponse";
-export { default as Listing } from "./model/Listing";
+export { default as Listing, VehicleInfo, LeaseInfo } from "./model/Listing";
